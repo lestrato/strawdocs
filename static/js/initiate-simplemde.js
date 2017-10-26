5df03cd4eb25b2df520a5b619f1b234872f4ddd9
strato <clapama1@gmail.com>
2017-10-26 01:26:55 -0400

    simplemde no longer gets remove FA icons

--- static/js/initiate-simplemde.js
+++ static/js/initiate-simplemde.js
@@ -1,6 +1,7 @@
 
 function create_simplemde(element) {
   return new SimpleMDE ({
+   autoDownloadFontAwesome: false,
    element: document.getElementById(element),
    spellChecker: false,
    status: false,
