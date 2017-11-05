12ff61ffeccc64321bb5b6e22bdc0509d3b045dc
strato <clapama1@gmail.com>
2017-11-05 17:06:16 -0500

    static alert message/ tooltips/ thanks message for reporting

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,21 +1,21 @@
 function ajax_post(form, old_dom, new_dom){
       /* Take the the form, the dom ("#answers") for example, and the type of
       element to be saved */
+    $('#alert-absolute-div').html('');
     $.ajax({
         data: $(form).serialize(), // get the form data
         type: $(form).attr('method'), // GET or POST
         url: $(form).attr('action'), // the file to call
         // handle a successful response
         success : function(response) {
-            $('#alert-absolute-div').html('');
             $(old_dom).replaceWith($(response).find(new_dom));
+            $('[data-toggle="tooltip"]').tooltip();  // reinst. tooltips
         },
         // handle a non-successful response
         error : function() {
             $('#alert-absolute-div').html(
               '<div class="alert alert-danger text-center"><strong>Uh oh!</strong> Something went wrong on our side. <strong>Please try again later.</strong><button type="button" class="close" data-dismiss="alert">×</button></div>'
             )
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
         }
     });
 }
